8ba776f71e5564b77f82af4164231c82b11195ac
Kasha225 <86856944+Kasha225@users.noreply.github.com>
2024-02-27 00:02:13 +0500

    Add files via upload

---
+++ LABA_SALON1/src/main/java/org/example/FunctionsBD.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FunctionsBD {
+    public static void ClientsOut(Clients clients){
+        System.out.println(clients.getId_client() + " " + clients.getName() + " " + clients.getSurname());
+    }
+    public static void MastersOut(Masters masters){
+        System.out.println(masters.getId_master() + " " + masters.getName() + " " + masters.getSurname());
+    }
+    public static void AttentionOut(Attention at){
+        System.out.println(at.getId() + " " + at.getId_client() + " " + at.getId_master());
+    }
+}
